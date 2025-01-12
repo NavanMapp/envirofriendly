416706ea437091fad77f23e8d46302d85f6476d1
NavanMapp <navanmapp@gmail.com>
2025-01-12 19:03:11 +0200

    EnviroFriendly created, User Model created, recycletips

---
+++ src/main/java/com/enviro/assessment/grad001/navanmaphalala/EnvirofriendlyApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.navanmaphalala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvirofriendlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvirofriendlyApplication.class, args);
+	}
+
+}
